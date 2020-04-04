d188847f06272e60a68195c022713476827b05f3
KewinEvers96 <keversy@unal.edu.co>
2020-04-04 16:11:46 -0500

    correcionIndices

--- ipcAnual.py
+++ ipcAnual.py
@@ -4,30 +4,29 @@ import numpy as np
 ipcsAnuales = []
 years = []
 dictio = {}
-with open("1.1.INF_Serie histórica Meta de inflación IQY.xlsx", "rb") as file:
+#
+# Coger los indices de cada año del mes de enero. Y tomarlos como los inidices para el IPC anual
+#
+with open("1.2.5.IPC_Serie_variaciones.xlsx", "rb") as file:
     datos = pd.read_excel(file)
     datos = datos[6:]
     datosN = datos.to_numpy()
+    file.close()
+_ , column = np.where(datosN == "Inflación total 1")
 
-    _, column = np.where(datosN == "Inflación total 1")
+rowInit, _ = np.where(datosN == 201812)
+finalRow, _ = np.where(datosN == 199501)
+row0 = finalRow[0]
 
-    row, _ = np.where(datosN == 201812)
-
-    row0 = row[0]
-    
-    year = 2018
-    suma = 0
-    while(year >= 1995):
-        suma += datosN[row[0], column[0]]
-        row += 1
-        
-        if ((row[0] - row0) % 12) == 0:
-            ipcsAnuales.append(round(suma / 12, 4))
-            years.append(year)
-            suma = 0
-            year -= 1
-    dictio['Year'] = years
-    dictio['IPC'] = ipcsAnuales
+year = 1995
+suma = 0
+while(year <= 2018):
+    years.append(year)
+    ipcsAnuales.append(datosN[finalRow, 1][0])
+    finalRow -= 12
+    year += 1
+dictio['Year'] = years
+dictio['IPC'] = ipcsAnuales
 
 newDoct = pd.DataFrame(dictio, columns = ['Year', 'IPC'])
 newDoct.to_csv("IPCs_Anuales.csv")
--- scriptProcesamientoDatos.py
+++ scriptProcesamientoDatos.py
@@ -8,7 +8,19 @@ import matplotlib.pyplot as plt
 ##
 preciosDias = []
 max =0
+with open("datos/IPCs_Anuales.csv", "rb") as file:
+    ipcs = pd.read_csv(file)
+    ipcs = ipcs.to_numpy()[:, 1:]
+    file.close()
 
+ipcs[:, 1] = ipcs[:, 1] / 100
+n = ipcs.shape[0] -1
+precios = [100]
+
+while( n >= 0):
+    precios.append(round(precios[-1] * (ipcs[n, 1] + 1), 5))
+    n -= 1
+print(len(precios), ipcs.shape[0])
 for year in range(1995, 2016):
 # year = 1995
     with open("datos/precios/Precio_Bolsa_Nacional_($kwh)_"+ str(year) +".xlsx", "rb") as file:
@@ -16,34 +28,36 @@ for year in range(1995, 2016):
         datos = pd.read_excel(file)
         datosNumpy = datos.to_numpy()
         datosNumpy = np.nan_to_num(datosNumpy)
-        # Encontrar el valor "Fecha"
-        row, column = np.where(datosNumpy == "Fecha")
-        row +=1
-        column +=1
-        datosNumpy = datosNumpy[row[0]:, column[0]:]
-        if year >= 2010:
-            datosNumpy = datosNumpy[:, :-2]
-        if isinstance(datosNumpy[0, -1], str):
-            datosNumpy = datosNumpy[:, :-1]
-        for i in range(datosNumpy.shape[0]):
-            promedio = sum(datosNumpy[i,:])/ datosNumpy.shape[1]
-            preciosDias.append(promedio)
         file.close()
+        # Encontrar el valor "Fecha"
+    row, column = np.where(datosNumpy == "Fecha")
+    row +=1
+    column +=1
+    datosNumpy = datosNumpy[row[0]:, column[0]:]
+    if year >= 2010:
+        datosNumpy = datosNumpy[:, :-2]
+    if isinstance(datosNumpy[0, -1], str):
+        datosNumpy = datosNumpy[:, :-1]
+    for i in range(datosNumpy.shape[0]):
+        promedio = sum(datosNumpy[i,:])/ datosNumpy.shape[1]
+        # Deflectamos los precios
+        preciosDias.append(promedio)
+    
 for year in range(2016, 2019):
     with open("datos/precios/Precio_Bolsa_Nacional_($kwh)_" + str(year) + ".xls", "rb") as file:
         datos = pd.read_excel(file)
         datosNumpy = datos.to_numpy()
         datosNumpy = np.nan_to_num(datosNumpy)
-
-        row, column = np.where(datosNumpy == "Fecha")
-        row += 1
-        column += 1
-        datosNumpy = datosNumpy[row[0]: , column[0]:]
-        datosNumpy = datosNumpy[:, :-2]
-        
-        for i in range(datosNumpy.shape[0]):
-            promedio = sum(datosNumpy[i, :]) / datosNumpy.shape[1]
-            preciosDias.append(promedio)
+        file.close()
+    row, column = np.where(datosNumpy == "Fecha")
+    row += 1
+    column += 1
+    datosNumpy = datosNumpy[row[0]: , column[0]:]
+    datosNumpy = datosNumpy[:, :-2]
+    
+    for i in range(datosNumpy.shape[0]):
+        promedio = sum(datosNumpy[i, :]) / datosNumpy.shape[1]
+        preciosDias.append(promedio)
 
         file.close()
 plt.title("Precios entre 1995 - 2018")
